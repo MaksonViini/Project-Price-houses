f6b3a1d09e46a285a770eb8b5e58752a7f86e8c4
MaksonViini <maksonvinicio7@gmail.com>
2021-03-02 12:30:31 -0300

    Mudando o arquivo app.py e adicionando o arquivo csv

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 # Aqui vai ter o APP
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+print('Deploy ainda a ser feito')
+
+# título
+st.title("Sistema de Previsão de Risco do Cliente para Concessão de Empréstimos- By Makson Vinicio")
+
+# subtítulo
+st.markdown("Este é um Aplicativo utilizado para exibir a solução de Ciência de Dados para o problema de predição de Risco do Cliente para concessão de empréstimos.")
+
+st.sidebar.subheader("Insira os Dados dos Clientes para Análise do Risco")
