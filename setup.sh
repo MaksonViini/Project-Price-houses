891f71c7e69139502268c7ea538f11de73d323ee
MaksonViini <maksonvinicio7@gmail.com>
2021-03-02 12:45:29 -0300

    Mudando setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 mkdir -p ~/.streamlit/
 
-echo "\[general]\n\email = \"maksonvinicio7@gmail.com\"\n\" > ~/.streamlit/credentials.toml
-
-echo "\[server]\n\headless = true\n\enableCORS=false\n\port = $PORT\n\" > ~/.streamlit/config.toml
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
