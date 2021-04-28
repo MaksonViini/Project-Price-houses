c16745425ab5f4dcdcf0d991f584e43c5a30f2d9
MaksonViini <maksonvinicio7@gmail.com>
2021-04-28 20:02:58 -0300

    Criando data app multi page

---
+++ apps/dataapp.py
@@ -0,0 +1,15 @@
+# Aqui vai ter o APP
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+print('Deploy ainda a ser feito')
+
+# título
+st.title("Sistema de Previsão da House Rocket- By Makson Vinicio")
+
+# subtítulo
+st.markdown("Este é um Aplicativo utilizado para exibir a solução de Ciência de Dados para o problema de predição de Compra de imoveis.")
+
+st.sidebar.subheader("Bar")
+st.sidebar.file_
